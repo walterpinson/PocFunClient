5516b0323313c83a4eaa6a5cf65addd3792bf1dd
tmaxwell <taralmaxwell@gmail.com>
2013-07-29 16:28:41 -0400

    Added new logic for token getting from URL and routing

--- src/angular/app/scripts/app.js
+++ src/angular/app/scripts/app.js
@@ -3,12 +3,8 @@
 angular.module('poc', ['ui.bootstrap', 'poc.jobApp.list', 'poc.service.jobs', 'poc.jobApp.temp'])
   .config(function ($routeProvider) {
     $routeProvider
-        .when('/#t', {
-            templateUrl: 'apps/pocfun/views/job-list.html' + "hello.dude",
-            controller: 'listCtrl'
-        })
-        .when('/', {
-            templateUrl: 'apps/pocfun/views/job-list.html' + $scope.token,
+        .when('/:t', {
+            templateUrl: 'apps/pocfun/views/job-list.html',
             controller: 'listCtrl'
         })
       .otherwise({
