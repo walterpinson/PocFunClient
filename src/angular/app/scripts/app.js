683c8296c1f575d55755e22171085d7a1e076bc7
Walter Pinson <walter@walterpinson.com>
2013-07-29 12:54:08 -0400

    Playing around with routes.

--- src/angular/app/scripts/app.js
+++ src/angular/app/scripts/app.js
@@ -3,10 +3,14 @@
 angular.module('poc', ['poc.jobApp.list', 'poc.service.jobs', 'poc.jobApp.temp'])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'apps/pocfun/views/job-list.html',
-        controller: 'listCtrl'
-      })
+        .when('/#t', {
+            templateUrl: 'apps/pocfun/views/job-list.html' + "hello.dude",
+            controller: 'listCtrl'
+        })
+        .when('/', {
+            templateUrl: 'apps/pocfun/views/job-list.html' + $scope.token,
+            controller: 'listCtrl'
+        })
       .otherwise({
         redirectTo: '/'
       });
