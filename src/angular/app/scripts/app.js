06a6f2d8fe8a82205ba47479710d4a4ab62f0488
tmaxwell <taralmaxwell@gmail.com>
2013-07-31 17:28:55 -0400

    TA67843 - Implemented new routing with conditional routes based on $location so that AngularJS app can run independently from .Net client solution

--- src/angular/app/scripts/app.js
+++ src/angular/app/scripts/app.js
@@ -3,11 +3,21 @@
 angular.module('poc', ['ui.bootstrap', 'poc.jobApp.list', 'poc.service.jobs', 'poc.jobApp.temp'])
   .config(function ($routeProvider) {
     $routeProvider
-        .when('/:t', {
-            templateUrl: 'apps/pocfun/views/job-list.html',
-            controller: 'listCtrl'
-        })
+      .when('/:t', {
+        templateUrl: 'job-list.html',
+        controller: 'listCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function ($rootScope,$location){
+    $rootScope.$on('$routeChangeStart',function(event,next,current){
+      if ($location.host() == 'localhost') {
+        next.templateUrl = "views/"+next.templateUrl;
+      } else {
+        next.templateUrl = "apps/pocfun/views/"+next.templateUrl;
+      }
+    });
   });
+
